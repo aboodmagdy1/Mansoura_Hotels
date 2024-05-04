85877fd3802801612bf2763d514441dfd8b87ab7
Abood Magdy <c.aboodmagdy@gmail.com>
2024-05-04 10:06:54 +0300

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,5 @@
+import { Layout } from "./layouts/layout";
+
+export function App() {
+  return <Layout />;
+}
---
+++ frontend/src/layouts/layout.tsx
@@ -0,0 +1,8 @@
+import { Header } from "../components/Header";
+export const Layout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+    </div>
+  );
+};
