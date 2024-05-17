3811e192315cf0ae039136f70d7c8984b2102b31
Abood Magdy <c.aboodmagdy@gmail.com>
2024-05-17 12:04:17 +0300

    fix: set static frontend path

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
+import path from "path";
 
 // connect to DB vefore every thing
 mongoose
@@ -24,10 +25,11 @@ app.use(
     credentials: true,
   })
 );
-
+//define the fontend folder to be served by default
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
 app.listen(4000, () => {
-  console.log(`Server is running on port 3000`);
+  console.log(`Server is running on port 4000`);
 });
