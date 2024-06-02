47e42ac93938c050c8ebcc33e871ba798fef69ef
Abood Magdy <c.aboodmagdy@gmail.com>
2024-06-02 12:19:09 +0300

    feat :handel save process

--- frontend/src/forms/ManageHotelForms/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForms/ManageHotelForm.tsx
@@ -15,15 +15,37 @@ export type HotelFormData = {
   starRating: number;
   facilities: string[];
   imageFiles: FileList;
-  adultCount: string;
-  childCount: string;
+  adultCount: number;
+  childCount: number;
+};
+type Props = {
+  onSave: (hotelFormData: FormData) => void;
+  isLoading: boolean;
 };
 
-const ManageHotelForm = () => {
+const ManageHotelForm = ({ onSave, isLoading }: Props) => {
   const formMethods = useForm<HotelFormData>();
   const { handleSubmit } = formMethods;
   const OnSubmit = handleSubmit((data: HotelFormData) => {
-    console.log(data);
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("city", data.city);
+    formData.append("country", data.country);
+    formData.append("description", data.description);
+    formData.append("type", data.type);
+    formData.append("pricePerNight", data.pricePerNight.toString());
+    formData.append("starRating", data.starRating.toString());
+    formData.append("adultCount", data.adultCount.toString());
+    formData.append("childCount", data.childCount.toString());
+
+    data.facilities.forEach((facility, index) => {
+      formData.append(`facilities[${index}]`, facility);
+    });
+
+    Array.from(data.imageFiles).forEach((imageFile) => {
+      formData.append(`imageFiles`, imageFile);
+    });
+    onSave(formData);
   });
   return (
     <FormProvider {...formMethods}>
@@ -38,7 +60,7 @@ const ManageHotelForm = () => {
             type="submit"
             className="border rounded bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
           >
-            Save
+            {isLoading ? "Saving..." : "Save"}
           </button>
         </span>
       </form>
--- frontend/src/pages/addHotel.tsx
+++ frontend/src/pages/addHotel.tsx
@@ -1,7 +1,23 @@
+import { useAppContext } from "../contexts/AppContext";
 import ManageHotelForm from "../forms/ManageHotelForms/ManageHotelForm";
+import { useMutation } from "react-query";
+import * as apiClient from "../api-client";
 
 const AddHotel = () => {
-  return <ManageHotelForm />;
+  const { showMessage } = useAppContext();
+  const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
+    onSuccess: () => {
+      showMessage({ message: "Hotell added successfully", type: "SUCCESS" });
+    },
+    onError: () => {
+      showMessage({ message: "Failed to  add hotel", type: "ERROR" });
+    },
+  });
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+  return <ManageHotelForm onSave={handleSave} isLoading={isLoading} />;
 };
 
 export default AddHotel;
