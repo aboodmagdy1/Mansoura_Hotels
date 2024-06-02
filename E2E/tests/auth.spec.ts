3430497a2555b01765be5937ef94544424079b68
Abood Magdy <c.aboodmagdy@gmail.com>
2024-06-02 21:15:28 +0300

    feat:testing for adding hotel

--- E2E/tests/auth.spec.ts
+++ E2E/tests/auth.spec.ts
@@ -19,7 +19,7 @@ test("should allow the user to sign in ", async ({ page }) => {
   await page.getByRole("button", { name: "Login" }).click();
 
   //assertion after login
-  await expect(page.getByText("Sign In Success")).toBeVisible();
+  await expect(page.getByText("logged in successfuly")).toBeVisible();
   await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
   await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
   await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
