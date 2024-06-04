7ccdcf0b00621b5a09bccc31ce54d29ca0520d3a
Abood Magdy <c.aboodmagdy@gmail.com>
2024-06-04 09:04:54 +0300

    fix:navigate after success

--- frontend/src/pages/addHotel.tsx
+++ frontend/src/pages/addHotel.tsx
@@ -2,12 +2,15 @@ import { useAppContext } from "../contexts/AppContext";
 import ManageHotelForm from "../forms/ManageHotelForms/ManageHotelForm";
 import { useMutation } from "react-query";
 import * as apiClient from "../api-client";
+import { useNavigate } from "react-router-dom";
 
 const AddHotel = () => {
   const { showMessage } = useAppContext();
+  const navigate = useNavigate();
   const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
     onSuccess: () => {
       showMessage({ message: "Hotell added successfully", type: "SUCCESS" });
+      navigate("/my-hotels");
     },
     onError: () => {
       showMessage({ message: "Failed to  add hotel", type: "ERROR" });
