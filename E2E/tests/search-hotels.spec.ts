ac069f9174a70386f34bae3bbba1dc1648012de5
Abood Magdy <c.aboodmagdy@gmail.com>
2024-06-18 11:55:15 +0300

    test: search page test

---
+++ E2E/tests/search-hotels.spec.ts
@@ -0,0 +1,26 @@
+import { expect, test } from "@playwright/test";
+const UI_URL = "http://localhost:5173";
+
+// sign in before each request
+test.beforeEach(async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByRole("link", { name: "Sign In" }).click();
+
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+  await page.locator("[name='email']").fill("admin@gmail.com");
+  await page.locator("[name='password']").fill("adminpass");
+
+  await page.getByRole("button", { name: "LogIn" }).click();
+
+  await expect(page.getByText("logged in successfuly")).toBeVisible();
+});
+
+test("Should show hotel search results", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("Where are you going").fill("Test City");
+  await page.getByRole("button", { name: "Search" }).click();
+  await expect(page.getByText("1 Hotels foundin Test City")).toBeVisible();
+  await expect(page.getByText("Test Hotel Name Updated")).toBeVisible();
+});
