96353a8d591a1ca0811a5d3de4094301dfb43b1c
Abood Magdy <c.aboodmagdy@gmail.com>
2024-06-18 09:25:40 +0300

    feat: filter and sort

--- backend/src/controllers/hotels.ts
+++ backend/src/controllers/hotels.ts
@@ -13,7 +13,27 @@ export const getHotels = async (req: Request, res: Response) => {
     const skip = (pageNumber - 1) * pageSize; // skip this number to get the next to them
     const total = await Hotel.countDocuments();
 
-    const hotels = await Hotel.find().skip(skip).limit(pageSize);
+    //for filtering purposes
+    const query = constructSearchQuery(req.query);
+
+    //sorting
+    let sortOptions = {};
+    switch (req.query.sortOption) {
+      case "startRating":
+        sortOptions = { startRating: -1 };
+        break;
+      case "pricePerNightAsc":
+        sortOptions = { pricePerNight: 1 };
+        break;
+      case "pricePerNightDesc":
+        sortOptions = { pricePerNight: -1 };
+        break;
+    }
+
+    const hotels = await Hotel.find(query)
+      .sort(sortOptions)
+      .skip(skip)
+      .limit(pageSize);
     const response: HotelSearchResponse = {
       data: hotels,
       pagination: {
@@ -27,4 +47,59 @@ export const getHotels = async (req: Request, res: Response) => {
     console.log("error", error);
     res.status(500).json({ message: "Something  went wrong" });
   }
+};
+
+const constructSearchQuery = (queryParams: any) => {
+  let constructedQuery: any = {};
+
+  if (queryParams.destination) {
+    constructedQuery.$or = [
+      { city: new RegExp(queryParams.destination, "i") },
+      { country: new RegExp(queryParams.destination, "i") },
+    ];
+  }
+
+  if (queryParams.adultCount) {
+    constructedQuery.adultCount = {
+      $gte: parseInt(queryParams.adultCount),
+    };
+  }
+
+  if (queryParams.childCount) {
+    constructedQuery.childCount = {
+      $gte: parseInt(queryParams.childCount),
+    };
+  }
+
+  if (queryParams.facilities) {
+    constructedQuery.facilities = {
+      $all: Array.isArray(queryParams.facilities)
+        ? queryParams.facilities
+        : [queryParams.facilities],
+    };
+  }
+
+  if (queryParams.types) {
+    constructedQuery.type = {
+      $in: Array.isArray(queryParams.types)
+        ? queryParams.types
+        : [queryParams.types],
+    };
+  }
+
+  if (queryParams.stars) {
+    const starRatings = Array.isArray(queryParams.stars)
+      ? queryParams.stars.map((star: string) => parseInt(star))
+      : [parseInt(queryParams.stars)];
+
+    constructedQuery.starRating = { $in: starRatings };
+  }
+
+  if (queryParams.maxPrice) {
+    constructedQuery.pricePerNight = {
+      $lte: parseInt(queryParams.maxPrice).toString(),
+    };
+  }
+
+  return constructedQuery;
 };
