2826a3be61abbc1e9a4d35a20fe7e5e27c5677d4
Abood Magdy <c.aboodmagdy@gmail.com>
2024-06-20 12:25:16 +0300

    feat: detail page test

--- E2E/tests/search-hotels.spec.ts
+++ E2E/tests/search-hotels.spec.ts
@@ -24,3 +24,14 @@ test("Should show hotel search results", async ({ page }) => {
   await expect(page.getByText("1 Hotels foundin Test City")).toBeVisible();
   await expect(page.getByText("Test Hotel Name Updated")).toBeVisible();
 });
+
+test("Should show hotel detail", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("Where are you going").fill("Test City");
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await page.getByText("Test Hotel Name Updated").click();
+  await expect(page).toHaveURL(/detail/);
+  await expect(page.getByRole("button", { name: "Book Now" })).toBeVisible();
+});
