1d01c147f48849ab533a1a844198476615a8c358
Abood Magdy <c.aboodmagdy@gmail.com>
2024-06-20 11:36:35 +0300

    fix: fixing show search bar without log in

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -2,23 +2,19 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import SearchBar from "../components/SearchBar";
-import { useAppContext } from "../contexts/AppContext";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: Props) => {
-  const { isLoggedIn } = useAppContext();
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <Hero />
-      {isLoggedIn && (
-        <div className="container mx-auto">
-          <SearchBar />
-        </div>
-      )}
+      <div className="container mx-auto">
+        <SearchBar />
+      </div>
       <div className="container mx-auto py-10 flex-1 ">{children}</div>
       <Footer />
     </div>
