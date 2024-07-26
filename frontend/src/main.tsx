ed6e24664e7c6f218f60914845236dd318247894
Abood Magdy <c.aboodmagdy@gmail.com>
2024-07-26 14:45:32 +0300

    fix: bug

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "./index.css";
 import App from "./App.tsx";
@@ -13,7 +13,8 @@ const queryClient = new QueryClient({
     },
   },
 }); // fro making fetch requests
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = createRoot(document.getElementById("root")!);
+root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AppContextProvider>
